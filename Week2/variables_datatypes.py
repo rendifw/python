df7324d92d7637e7bb66045ce9de1424d9486665
Rendi <rendi.wikarta@binus.ac.id>
2025-02-10 04:29:55 +0000

    Added Week 3 and exercise image

--- Week2/variables_datatypes.py
+++ Week2/variables_datatypes.py
@@ -1,9 +1,15 @@
-myString = "Hello, World!"
-myInteger = 5
-myFloat = 3.87
 
+
+# The example string variables and display it
+myString = "Hello, World!" 
 print(myString)
+
+# The example Integer variables and display it
+myInteger = 5
 print(myInteger)
+
+# The example Float variables and display it
+myFloat = 3.87
 print(myFloat)
 
-# This is a comment
+# This is a comment``
