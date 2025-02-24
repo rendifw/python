88c54cc8c7f747cc0c454dafc8159a3c8a8026c7
Rendi <rendi.wikarta@binus.ac.id>
2025-02-24 06:01:47 +0000

    week 4

---
+++ Week4/list.py
@@ -0,0 +1,64 @@
+
+''' First Question
+names = ["Lisa", "Yoona", "Ryan"]
+for name in names:
+    print(f"{name}, mendapat nilai sempurna!")
+    print(f"Nilai kamu buruk sekali, {name} \n")
+
+score = "great"
+print(f"Proud of you All, you are so {score.upper()}!")
+8 Poin
+'''
+
+
+''' Second Question
+genap = list(range(4, 21, 6))
+ganjil = list(range(1, 11, 3))
+print(genap)
+print(ganjil)
+3 Poin
+'''
+
+
+''' Third Question
+pangkat = []
+for num in range(1, 255):
+    value = num ** 5
+    pangkat.append(value)
+print(pangkat)
+5 Poin
+'''
+
+''' Fourth Question
+range = [5, 10, 25, 75, 100, 150, 155, 250, 255, 300]
+print(min(range))
+print(max(range))
+print(sum(range))
+average = sum(range) / len(range)
+print(average)
+5 Poin
+'''
+
+
+''' Fifth Question
+jurusan = ['SOIS', 'SOCS', 'BBS', 'law', 'DCS', 'DRM']
+# print(jurusan[2:6])
+for x in jurusan[2:6]:
+    print(x)
+5 Poin
+'''
+
+
+
+''' Sixth Question 
+nama = ("vincent", "eric")
+print("Original Names:")
+for x in nama:
+    print(x)
+
+nama = ("yohanes", "dian", "flo")
+print("\nModified Names: ")
+for y in nama:
+    print(y)
+5 Poin
+'''
